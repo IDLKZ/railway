b7eb58033df3e67084ed8c44e8ec1de83b1cf882
IDLKZ <idlkz2019@gmail.com>
2020-08-31 22:42:54 +0600

    йц

---
+++ resources/views/frontend/docs.blade.php
@@ -0,0 +1,72 @@
+@extends("layout")
+
+@section("content")
+
+    <!-- ======= Testimonials Section ======= -->
+    <section id="testimonials" class="testimonials section-bg">
+        <div class="container">
+
+            <div class="section-title mt-2">
+                <span>Документы</span>
+                <h2>Документы</h2>
+            </div>
+
+           <div class="container">
+           <div class="row mt-4 docs">
+              <div class="col-md-2 col-sm-12 text-center docs-div-icon d-none d-sm-block" >
+                  <i class="icofont-file-document docs-icon" ></i>
+              </div>
+              <div class="col-md-8 col-sm-12">
+                  <h3 class="docs-text" >Типовой договор по заявке на ремонт</h3>
+              </div>
+               <div class="col-md-2 col-sm-12 text-center docs-div-icon">
+                  <a href="example.docx" download > <i class="icofont-file-word docs-icon" ></i></a>
+               </div>
+
+
+
+           </div>
+           <div class="row mt-4 docs" >
+              <div class="col-md-2 col-sm-12 text-center docs-div-icon d-none d-sm-block" >
+                  <i class="icofont-file-document docs-icon" ></i>
+
+              </div>
+              <div class="col-md-8 col-sm-12">
+                  <h3 class="docs-text" >Гарантийное письмо</h3>
+              </div>
+               <div class="col-md-2 col-sm-12 text-center docs-div-icon">
+                  <a href="example.pdf" download > <i class="icofont-file-pdf docs-icon" ></i></a>
+               </div>
+
+
+
+           </div>
+           <div class="row mt-4 docs">
+              <div class="col-md-2 col-sm-12 text-center docs-div-icon d-none d-sm-block" >
+                  <i class="icofont-file-document docs-icon" ></i>
+
+              </div>
+              <div class="col-md-8 col-sm-12">
+                  <h3 class="docs-text" >Калькуляция</h3>
+              </div>
+               <div class="col-md-2 col-sm-12 text-center docs-div-icon">
+                  <a href="example.docx" download> <i class="icofont-file-word docs-icon" ></i></a>
+               </div>
+
+
+
+           </div>
+
+
+
+           </div>
+
+        </div>
+    </section><!-- End Testimonials Section -->
+
+    <!-- ======= Cta Section ======= -->
+
+
+
+
+@endsection
