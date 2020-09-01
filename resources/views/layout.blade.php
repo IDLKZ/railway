446ac85078834aa814e28b7ec86a2452a18a59d6
idlkz <idlkz2019@gmail.com>
2020-09-01 22:39:42 +0600

    asd

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -121,7 +121,7 @@
 
     <div class="container footer-bottom clearfix">
         <div class="copyright">
-            &copy; Copyright <strong><span>Имя компании</span></strong>. All Rights Reserved
+            &copy; Copyright <strong><span>Вагонный сервис</span></strong>. All Rights Reserved
         </div>
 {{--        <div class="credits">--}}
 {{--            Разработано веб-студией <a href="https://idl.kz">IDL</a>--}}
